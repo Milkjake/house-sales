94b760ac5de860fa9984c80950cd7f26a8966888
Jacques <jdutoit49@gmail.com>
2021-07-02 16:47:26 +1200

    Update text formatter, add sale max to house request, add sale max and limit to env

--- cards/cards-request-payload.js
+++ cards/cards-request-payload.js
@@ -1,12 +1,13 @@
 const cardsRequestPayload = {
   polylines: [],
   page: 1,
-  limit: 20,
+  limit: process.env.LIMIT,
   display_rentals: false,
   for_rent: false,
   for_sale: true,
   just_sold: false,
   off_market: false,
+  sale_max: process.env.SALE_MAX,
   sort: { order: "date", direction: "asc" },
 };
 
--- mailer/textFormatter.js
+++ mailer/textFormatter.js
@@ -1,19 +1,34 @@
 exports.textFormatter = function (suburbsData) {
-  return suburbsData.reduce((accText, curSuburb) => {
-    const { suburb, city, properties } = curSuburb;
-    accText += `${suburb}, ${city}\n`;
-    accText += properties.reduce((accPropertyText, curProperty) => {
-      const { address, url, dateAdded, estimateValue, estimateRange } =
-        curProperty;
+  return suburbsData.reduce((accText, currSuburb) => {
+    const { suburb, city, properties } = currSuburb;
 
-      accPropertyText += `\t address: ${address}\n 
-      \t url: ${url}\n 
-      \t date added: ${dateAdded} \n 
-      \t estimated value: ${estimateValue} \n 
-      \t estimated range: ${estimateRange}\n`;
+    if (properties.length > 0) {
+      accText += suburb + ", " + city + "\n";
+      accText += properties.reduce((accPropertyText, currProperty) => {
+        const { address, url, dateAdded, estimateValue, estimateRange } =
+          currProperty;
 
-      return accPropertyText;
-    }, "");
-    return accText;
+        accPropertyText +=
+          "\t address: " +
+          address +
+          "\n" +
+          "\t url: " +
+          url +
+          "\n" +
+          "\t date added: " +
+          dateAdded +
+          "\n" +
+          "\t estimated value:" +
+          estimateValue +
+          "\n" +
+          "\t estimated range:" +
+          estimateRange +
+          "\n\n";
+
+        return accPropertyText;
+      }, "");
+      return accText;
+    }
+    return "";
   }, "");
 };
