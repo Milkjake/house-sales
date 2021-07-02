2994e87025bc2aec81d8517d20f7af88dc8a770b
Jacques <jdutoit49@gmail.com>
2021-07-02 16:58:21 +1200

    Parse env var to int

--- cards/cards-request-payload.js
+++ cards/cards-request-payload.js
@@ -1,13 +1,13 @@
 const cardsRequestPayload = {
   polylines: [],
   page: 1,
-  limit: process.env.LIMIT,
+  limit: parseInt(process.env.LIMIT),
   display_rentals: false,
   for_rent: false,
   for_sale: true,
   just_sold: false,
   off_market: false,
-  sale_max: process.env.SALE_MAX,
+  sale_max: parseInt(process.env.SALE_MAX),
   sort: { order: "date", direction: "asc" },
 };
 
